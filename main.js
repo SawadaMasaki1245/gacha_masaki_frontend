7408cb866cd6a7cdb32d107f86a2c373f5ca9310
澤田匡輝 <m.sawada.sys23@morijyobi.ac.jp>
2025-07-16 10:17:19 +0900

    first-push

---
+++ main.js
@@ -0,0 +1,6 @@
+document.getElementById('single-gacha').onclick = () => {
+  document.getElementById('result').textContent = '単発ガチャがクリックされました';
+};
+document.getElementById('ten-gacha').onclick = () => {
+  document.getElementById('result').textContent = '10連ガチャがクリックされました';
+};
